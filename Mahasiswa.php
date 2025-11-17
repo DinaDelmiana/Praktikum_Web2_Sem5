3782c782ea934ee26ebf1b648d8fcdf77177d343
DinaDelmiana <dndl.3139@gmail.com>
2025-11-17 16:33:59 +0800

    MODUL 1: KONSEP DASAR OOP DI PHP

---
+++ Mahasiswa.php
@@ -0,0 +1,13 @@
+<?php
+class Mahasiswa
+{
+               public $nama;
+               public $nim;
+
+
+public function sayHello()
+{
+               echo "Halo, saya adalah Mahasiswa " . $this->nama . "<br>";
+               echo "NIM saya adalah " . $this->nim . ". Selamat datang di dunia OOP!";
+}
+}
