3ef99a28a2eb1ef19727388380446efc58dda66f
DinaDelmiana <dndl.3139@gmail.com>
2025-11-24 17:01:17 +0800

    MODUL 7: CORE DAN ROUTING DASAR

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+ // ===============================================
+ // FRONT CONTROLLER - TITIK MASUK UTAMA APLIKASI
+ // ===============================================
+ // Pastikan file Router (App.php) dimuat
+ require_once '../app/core/App.php';
+
+ // Instansiasi dan jalankan App (Router)
+$app = new App;
